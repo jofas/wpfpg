2d2e474c7eb58f7ecf7ece4ed13646702a39fe8b
jofas <jonas@fc-web.de>
2019-02-11 16:16:45 +0100

    task_day0

---
+++ task_day0/Assets/Triangle/Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle : MonoBehaviour
+{
+  public Vector3 p0 = Vector3.zero;
+  public Vector3 p1 = Vector3.forward;
+  public Vector3 p2 = Vector3.right;
+
+  public MeshFilter mesh_f;
+  public MeshRenderer mesh_r;
+  public Mesh mesh;
+
+  public void CreateMesh() {
+    mesh_f = this.gameObject.GetComponent<MeshFilter>();
+    if (mesh_f == null) {
+      mesh_f = this.gameObject.AddComponent<MeshFilter>();
+    }
+
+    mesh_r = this.gameObject.GetComponent<MeshRenderer>();
+    if (mesh_r == null) {
+      mesh_r =
+        this.gameObject.AddComponent<MeshRenderer>();
+    }
+
+    mesh = mesh_f.sharedMesh;
+    if (mesh == null) {
+      mesh = new Mesh();
+      mesh.name = "Triangle";
+    }
+
+    Vector3[] vertices = new Vector3[] { p0, p1, p2 };
+    int[] triangles = new int[] { 0, 1, 2 };
+
+    mesh.Clear();
+    mesh.vertices = vertices;
+    mesh.triangles = triangles;
+
+    mesh_f.sharedMesh = mesh;
+  }
+
+  // OnDrawGizmos {{{
+  void OnDrawGizmos() {
+    Vector3 p0_trans = transform.TransformPoint(p0);
+    Vector3 p1_trans = transform.TransformPoint(p1);
+    Vector3 p2_trans = transform.TransformPoint(p2);
+
+    Gizmos.DrawLine(p0_trans, p1_trans);
+    Gizmos.DrawLine(p0_trans, p2_trans);
+    Gizmos.DrawLine(p1_trans, p2_trans);
+
+    Gizmos.color = Color.red;
+    Gizmos.DrawSphere(p0_trans, 0.1f);
+
+    Gizmos.color = Color.green;
+    Gizmos.DrawSphere(p1_trans, 0.1f);
+
+    Gizmos.color = Color.blue;
+    Gizmos.DrawSphere(p2_trans, 0.1f);
+
+    CreateMesh();
+  }
+  // }}}
+}
---
+++ task_day0/Assets/_Quad/Editor/_QuadEditor.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(_Quad))]
+public class _QuadEditor : Editor
+{
+  private static bool draw_default = false;
+
+  public _Quad quad;
+
+  void Awake() {
+    quad = target as _Quad;
+  }
+
+  [MenuItem("GameObject/3D Object/_Quad")]
+  public static _Quad CreateQuad() {
+    GameObject go = new GameObject("_Quad");
+    _Quad quad = go.AddComponent<_Quad>();
+    quad.CreateMesh();
+    return quad;
+  }
+
+  public void OnSceneGUI() {
+
+    Vector3 p0_trans = quad.transform
+      .TransformPoint(quad.p0);
+
+    Vector3 p1_trans = quad.transform
+      .TransformPoint(quad.p1);
+
+    Vector3 p2_trans = quad.transform
+      .TransformPoint(quad.p2);
+
+    Vector3 p3_trans = quad.transform
+      .TransformPoint(quad.p3);
+
+    quad.p0 = Handles.FreeMoveHandle(
+      p0_trans,
+      Quaternion.identity,
+      0.1f,
+      Vector3.zero,
+      Handles.SphereHandleCap
+    );
+
+    quad.p0 = quad.transform
+      .InverseTransformPoint(quad.p0);
+
+    quad.p1 = Handles.FreeMoveHandle(
+      p1_trans,
+      Quaternion.identity,
+      0.1f,
+      Vector3.zero,
+      Handles.SphereHandleCap
+    );
+
+    quad.p1 = quad.transform
+      .InverseTransformPoint(quad.p1);
+
+    quad.p2 = Handles.FreeMoveHandle(
+      p2_trans,
+      Quaternion.identity,
+      0.1f,
+      Vector3.zero,
+      Handles.SphereHandleCap
+    );
+
+    quad.p2 = quad.transform
+      .InverseTransformPoint(quad.p2);
+
+    quad.p3 = Handles.FreeMoveHandle(
+      p3_trans,
+      Quaternion.identity,
+      0.1f,
+      Vector3.zero,
+      Handles.SphereHandleCap
+    );
+
+    quad.p3 = quad.transform
+      .InverseTransformPoint(quad.p3);
+  }
+
+  public override void OnInspectorGUI() {
+    quad.p0 = EditorGUILayout
+      .Vector3Field("p0", quad.p0);
+    quad.p1 = EditorGUILayout
+      .Vector3Field("p1", quad.p1);
+    quad.p2 = EditorGUILayout
+      .Vector3Field("p2", quad.p2);
+    quad.p3 = EditorGUILayout
+      .Vector3Field("p3", quad.p3);
+
+    if (draw_default = EditorGUILayout
+        .Foldout(draw_default, "Default")) {
+      DrawDefaultInspector();
+    }
+  }
+}
